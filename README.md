92e2078f5487d0f89dd79df9ec9340bdfecd94d9
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-09-13 01:51:02 -0400

    added uninstaller

---
+++ README.md
@@ -0,0 +1,41 @@
+<!----------------------------------------------------------------------------->
+<!-- Filename: README.md                                       /          \  -->
+<!-- Project : SpaceOddity                                    |     ()     | -->
+<!-- Author  : Dana Hynes                                     |   \____/   | -->
+<!-- License : WTFPLv2                                         \          /  -->
+<!----------------------------------------------------------------------------->
+
+# SpaceOddity
+## "It mostly works™"
+
+A program that changes your wallpaper to NASA's Astronomy Picture of the 
+Day.
+
+![](screenshot.jpg)
+
+# Installing
+
+You can download the
+[latest release](http://github.com/danahynes/SpaceOddity/releases/latest), 
+unzip it, and run the *install.py* file from there.
+
+You can also clone the git repo:
+```bash
+foo@bar:~$ cd ~/Downloads
+foo@bar:~$ git clone https://github.com/danahynes/SpaceOddity
+foo@bar: $ cd SpaceOddity
+foo@bar:~$ ./install.py
+```
+
+# Uninstalling
+You can download the
+[latest release](http://github.com/danahynes/SpaceOddity/releases/latest), 
+unzip it, and run the *uninstall.py* file from there.
+
+You can remove the files manually:
+``` bash
+foo@bar:~$ sudo rm -rf "~/.spacoddity"
+foo@bar:~$ sudo rm -rf "~/.config/spaceoddity"
+```
+
+# -)
--- uninstall.py
+++ uninstall.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 # -----------------------------------------------------------------------------#
-# Filename: install.py                                           /          \  #
+# Filename: uninstall.py                                         /          \  #
 # Project : SpaceOddity                                         |     ()     | #
 # Author  : Dana Hynes                                          |   \____/   | #
 # License : WTFPLv2                                              \          /  #
 # -----------------------------------------------------------------------------#
-
-# NEXT: run at screen unlock
-# https://unix.stackexchange.com/questions/28181/how-to-run-a-script-on-screen-lock-unlock
-# this means we don't need a cron job, except maybe just after midnight
-# also make cron job run every 10 minutes
 
 # ------------------------------------------------------------------------------
 # Imports
@@ -17,19 +12,14 @@
 
 import os
 import shlex
-import shutil
 import subprocess
-
-# TODO: an uninstaller that removes cron jobs
-# NEXT: run every hour,
-# NEXT: check date intead or url
 
 # ------------------------------------------------------------------------------
 # Define the main class
 # ------------------------------------------------------------------------------
 
 
-class Installer:
+class Uninstaller:
 
     # --------------------------------------------------------------------------
     # Methods
@@ -49,14 +39,10 @@ class Installer:
         # these are the values to set in preflight
         self.prog_name = ''
         self.run_as_root = False
-        self.run_after_install = False
 
         self.sys_reqs = []
         self.pip_reqs = []
         self.make_dirs = []
-        self.copy_files = {}
-
-        self.run_cmd = ''
 
     # --------------------------------------------------------------------------
     # Run the script
@@ -77,21 +63,16 @@ class Installer:
             exit()
 
         # show some text
-        print(f'Installing {self.prog_name}')
+        print(f'Uninstalling {self.prog_name}')
 
         # do the steps in order
         self.__do_preflight()
         self.__do_reqs()
         self.__make_dirs()
-        self.__copy_files()
         self.__do_postflight()
 
-        # done installing
-        print(f'{self.prog_name} installed')
-
-        # run the program now
-        if self.run_after_install:
-            self.__run_prog()
+        # done uninstalling
+        print(f'{self.prog_name} uninstalled')
 
     # --------------------------------------------------------------------------
     # Steps
@@ -108,7 +89,6 @@ class Installer:
 
         # set run vars
         # self.run_as_root = False # Default
-        self.run_after_install = True
 
         # system requirements
         self.sys_reqs = [
@@ -133,103 +113,67 @@ class Installer:
             cfg_dir
         ]
 
-        # copy files to dests
-        # NB: key is relative to src_dir, value is absolute
-        self.copy_files = {
-            f'{self.prog_name}.py': dst_dir,
-            'LICENSE':  dst_dir,
-            'VERSION': dst_dir,
-            'README.md': dst_dir
-        }
-
-        # the program to run after install
-        self.run_cmd = os.path.join(dst_dir, f'{self.prog_name}.py')
-
     # --------------------------------------------------------------------------
-    # Install prerequisites
+    # Remove prerequisites
     # --------------------------------------------------------------------------
     def __do_reqs(self):
 
         # show some text
-        print('Installing requirements')
+        print('Removing requirements')
 
         # get system requirements
         for item in self.sys_reqs:
 
             # show that we are doing something
-            print(f'Installing {item}')
+            print(f'Removing {item}')
 
-            # install apt reqs
-            cmd = f'sudo apt-get install {item}'
+            # iunnstall apt reqs
+            cmd = f'sudo apt-get remove {item}'
             cmd_array = shlex.split(cmd)
             try:
                 subprocess.run(cmd_array)
             except subprocess.CalledProcessError as error:
-                print(f'Could not install {item}:', error.stderr.decode())
+                print(f'Could not remove {item}:', error.stderr.decode())
                 exit()
 
         # get python requirements
         for item in self.pip_reqs:
 
             # show that we are doing something
-            print(f'Installing {item}')
+            print(f'Removing {item}')
 
-            # install pip reqs
-            cmd = f'pip install {item}'
+            # uninstall pip reqs
+            cmd = f'pip remove {item}'
             cmd_array = shlex.split(cmd)
             try:
                 subprocess.run(cmd_array)
             except subprocess.CalledProcessError as error:
-                print(f'Could not install {item}:', error.stderr.decode())
+                print(f'Could not remove {item}:', error.stderr.decode())
                 exit()
 
     # --------------------------------------------------------------------------
-    # Make any necessary directories
+    # Remove any necessary directories
     # --------------------------------------------------------------------------
     def __make_dirs(self):
 
         # show some text
-        print('Creating directories')
+        print('Removing directories')
 
         # for each folder we need to make
         for item in self.make_dirs:
 
             # show that we are doing something
-            print(f'Making directory {item}')
+            print(f'Removing directory {item}')
 
-            # make the folder(s)
+            # remove the folder(s)
             try:
-                os.makedirs(item, exist_ok=True)
+                os.remove(item)
             except OSError as error:
-                print(f'Could not create directory {item}:', error)
+                print(f'Could not remove directory {item}:', error)
                 exit()
 
     # --------------------------------------------------------------------------
-    # Copy all files to their dests
-    # --------------------------------------------------------------------------
-    def __copy_files(self):
-
-        # show some text
-        print('Copying files')
-
-        # for each file we need to copy
-        for key, val in self.copy_files.items():
-
-            # show that we are doing something
-            print(f'Copying {key} to {val}')
-
-            # convert relative path to absolute path
-            abs_key = os.path.join(self.src_dir, key)
-
-            # copy the file
-            try:
-                shutil.copy(abs_key, val)
-            except OSError as error:
-                print(f'Could not copy file {abs_key}:', error)
-                exit()
-
-    # --------------------------------------------------------------------------
-    # Set up crontab for changing wallpaper
+    # Remove crontab for changing wallpaper
     # --------------------------------------------------------------------------
     def __do_postflight(self):
 
@@ -237,70 +181,30 @@ class Installer:
         from crontab import CronTab
 
         # show some text
-        print('Creating cron job')
-
-        # set the job command
-        uid = os.getuid()
-        cron_cmd = 'env '
-        cron_cmd += 'DISPLAY=:0 '
-        cron_cmd += f'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/{uid}/bus '
-        cron_cmd += '/usr/bin/python3 '
-        cron_cmd += f'{self.run_cmd}'
+        print('Removing cron job')
 
         # get current user's crontab
         my_cron = CronTab(user=True)
 
-        # find 'every' job
-        my_job = None
+        # remove 'every' job
         for job in my_cron:
             if job.comment == f'{self.prog_name} every':
-                my_job = job
-                my_job.enable()
+                my_cron.remove(job)
 
-        # # create new job if neccesary
-        if my_job is None:
-            my_job = my_cron.new(command=cron_cmd,
-                                 comment=f'{self.prog_name} every')
-            my_job.minute.every(10)
-
-        # find 'reboot' job
-        my_job = None
+        # remove 'reboot' job
         for job in my_cron:
             if job.comment == f'{self.prog_name} reboot':
-                my_job = job
-                my_job.enable()
-
-        # # create new job if neccesary
-        if my_job is None:
-            my_job = my_cron.new(command=cron_cmd,
-                                 comment=f'{self.prog_name} reboot')
-            my_job.every_reboot()
+                my_cron.remove(job)
 
         # save job parameters
         my_cron.write()
-
-    # --------------------------------------------------------------------------
-    # Run the program after install
-    # --------------------------------------------------------------------------
-    def __run_prog(self):
-
-        # show some text
-        print(f'Running {self.prog_name}')
-
-        # run program now
-        cmd_array = shlex.split(self.run_cmd)
-        try:
-            subprocess.run(cmd_array)
-        except subprocess.CalledProcessError as error:
-            print(f'Could not run {self.prog_name}:', error.stderr.decode())
-            exit()
 
 
 # ------------------------------------------------------------------------------
 # Run the main class if we are not an import
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
-    installer = Installer()
-    installer.run()
+    uninstaller = Uninstaller()
+    uninstaller.run()
 
 # -)
