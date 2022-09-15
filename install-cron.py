62cd773c71de8c6e8ce73cfad23e0aafd98a54a9
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-09-14 20:10:16 -0400

    moved cron stuff to new file

---
+++ install-cron.py
@@ -0,0 +1,329 @@
+#!/usr/bin/env python3
+# -----------------------------------------------------------------------------#
+# Filename: install-cron.py                                      /          \  #
+# Project : SpaceOddity                                         |     ()     | #
+# Date    : 09/13/2022                                          |            | #
+# Author  : Dana Hynes                                          |   \____/   | #
+# License : WTFPLv2                                              \          /  #
+# -----------------------------------------------------------------------------#
+
+# ------------------------------------------------------------------------------
+# Imports
+# ------------------------------------------------------------------------------
+
+import os
+# import shlex
+# import shutil
+# import subprocess
+from crontab import CronTab
+
+# ------------------------------------------------------------------------------
+# Define the main class
+# ------------------------------------------------------------------------------
+
+
+class CronInstaller:
+
+    # --------------------------------------------------------------------------
+    # Methods
+    # --------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
+    # Initialize the class
+    # --------------------------------------------------------------------------
+    def __init__(self):
+
+        # get current user's home dir
+        self.home_dir = os.path.expanduser('~')
+
+        # get current dir
+        # self.src_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # these are the values to set in preflight
+        self.prog_name = ''
+        self.run_as_root = False
+    #     self.run_after_install = False
+
+    #     self.sys_reqs = []
+    #     self.pip_reqs = []
+    #     self.make_dirs = []
+    #     self.copy_files = {}
+
+    #     self.run_cmd = ''
+
+    # --------------------------------------------------------------------------
+    # Run the script
+    # --------------------------------------------------------------------------
+    def run(self):
+
+        # do the steps in order
+        self.__do_preflight()
+
+        # check for run as root/need to run as root
+        run_root = (os.geteuid() == 0)
+        if self.run_as_root and not run_root:
+            msg = 'This script needs to be run as root. '
+            msg += 'Try \'sudo ./install.py\''
+            print(msg)
+            exit()
+        elif not self.run_as_root and run_root:
+            msg = 'This script should not be run as root. '
+            msg += 'Try \'./install.py\''
+            print(msg)
+            exit()
+
+    #     # show some text
+    #     # NB: must be done after preflight to get self.prog_name
+    #     print(f'Installing {self.prog_name}')
+
+    #     self.__do_reqs()
+    #     self.__make_dirs()
+    #     self.__copy_files()
+        self.__do_postflight()
+
+    #     # done installing
+    #     print(f'{self.prog_name} installed')
+
+    #     # run the program now
+    #     if self.run_after_install:
+    #         self.__run_prog()
+
+    # --------------------------------------------------------------------------
+    # Steps
+    # --------------------------------------------------------------------------
+
+    # --------------------------------------------------------------------------
+    # Preflight - setup all variables from init
+    # --------------------------------------------------------------------------
+
+    def __do_preflight(self):
+
+        # the program name
+        self.prog_name = 'spaceoddity'
+
+        # # set run vars
+        # # self.run_as_root = False # Default
+        # self.run_after_install = True
+
+        # # system requirements
+        # self.sys_reqs = [
+        #     'python3-pip',
+        #     'imagemagick'
+        # ]
+
+        # # python requirements
+        # self.pip_reqs = [
+        #     'wand',
+        #     'python-crontab'
+        # ]
+
+        # # get some dirs
+        dst_dir = os.path.join(self.home_dir, f'.{self.prog_name}')
+        # cfg_dir = os.path.join(self.home_dir, '.config', f'{self.prog_name}')
+
+        # # make dirs
+        # # NB: these should be absolute paths
+        # self.make_dirs = [
+        #     dst_dir,
+        #     cfg_dir
+        # ]
+
+        # # copy files to dests
+        # # NB: key is relative to src_dir, value is absolute
+        # self.copy_files = {
+        #     'install.py': dst_dir,
+        #     'LICENSE':  dst_dir,
+        #     'README.md': dst_dir,
+        #     'screenshot.jpg': dst_dir,
+        #     f'{self.prog_name}.py': dst_dir,
+        #     'uninstall.py': dst_dir,
+        #     'VERSION': dst_dir
+        # }
+
+        # # the program to run after install
+        self.run_cmd = os.path.join(dst_dir, f'{self.prog_name}.py')
+
+        # # show some text
+        # print('Running update')
+
+        # # do update
+        # cmd = 'sudo apt-get update'
+        # cmd_array = shlex.split(cmd)
+        # try:
+        #     subprocess.run(cmd_array)
+        # except Exception as error:
+        #     print('Could not update:', error.stderr.decode())
+        #     exit()
+
+        # # show some text
+        # print('Running upgrade')
+
+        # # do upgrade
+        # cmd = 'sudo apt-get upgrade'
+        # cmd_array = shlex.split(cmd)
+        # try:
+        #     subprocess.run(cmd_array)
+        # except Exception as error:
+        #     print('Could not upgrade:', error.stderr.decode())
+        #     exit()
+
+    # --------------------------------------------------------------------------
+    # Install prerequisites
+    # --------------------------------------------------------------------------
+    # def __do_reqs(self):
+
+    #     # show some text
+    #     print('Installing requirements')
+
+    #     # get system requirements
+    #     for item in self.sys_reqs:
+
+    #         # show that we are doing something
+    #         print(f'Installing {item}')
+
+    #         # install apt reqs
+    #         cmd = f'sudo apt-get install {item}'
+    #         cmd_array = shlex.split(cmd)
+    #         try:
+    #             subprocess.run(cmd_array)
+    #         except Exception as error:
+    #             print(f'Could not install {item}:', error.stderr.decode())
+    #             exit()
+
+    #     # get python requirements
+    #     for item in self.pip_reqs:
+
+    #         # show that we are doing something
+    #         print(f'Installing {item}')
+
+    #         # install pip reqs
+    #         cmd = f'pip install {item}'
+    #         cmd_array = shlex.split(cmd)
+    #         try:
+    #             subprocess.run(cmd_array)
+    #         except Exception as error:
+    #             print(f'Could not install {item}:', error.stderr.decode())
+    #             exit()
+
+    # --------------------------------------------------------------------------
+    # Make any necessary directories
+    # --------------------------------------------------------------------------
+    # def __make_dirs(self):
+
+    #     # show some text
+    #     print('Creating directories')
+
+    #     # for each folder we need to make
+    #     for item in self.make_dirs:
+
+    #         # show that we are doing something
+    #         print(f'Making directory {item}')
+
+    #         # make the folder(s)
+    #         try:
+    #             os.makedirs(item, exist_ok=True)
+    #         except Exception as error:
+    #             print(f'Could not create directory {item}:', error)
+    #             exit()
+
+    # --------------------------------------------------------------------------
+    # Copy all files to their dests
+    # --------------------------------------------------------------------------
+    # def __copy_files(self):
+
+    #     # show some text
+    #     print('Copying files')
+
+    #     # for each file we need to copy
+    #     for key, val in self.copy_files.items():
+
+    #         # show that we are doing something
+    #         print(f'Copying {key} to {val}')
+
+    #         # convert relative path to absolute path
+    #         abs_key = os.path.join(self.src_dir, key)
+
+    #         # copy the file
+    #         try:
+    #             shutil.copy(abs_key, val)
+    #         except Exception as error:
+    #             print(f'Could not copy file {abs_key}:', error)
+    #             exit()
+
+    # --------------------------------------------------------------------------
+    # Set up crontab for changing wallpaper
+    # --------------------------------------------------------------------------
+    def __do_postflight(self):
+
+        # show some text
+        print('Creating cron job')
+
+        # set the job command
+        uid = os.getuid()
+        cron_cmd = 'env '
+        cron_cmd += 'DISPLAY=:0 '
+        cron_cmd += f'DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/{uid}/bus '
+        cron_cmd += '/usr/bin/python3 '
+        cron_cmd += f'{self.run_cmd}'
+
+        # get current user's crontab
+        my_cron = CronTab(user=True)
+
+        # find 'every' job
+        my_job = None
+        for job in my_cron:
+            if job.comment == f'{self.prog_name} every':
+                my_job = job
+
+        # # create new job if neccesary
+        if my_job is None:
+            my_job = my_cron.new(command=cron_cmd,
+                                 comment=f'{self.prog_name} every')
+
+        # set job time
+        my_job.enable()
+        my_job.minute.every(10)
+
+        # find 'reboot' job
+        my_job = None
+        for job in my_cron:
+            if job.comment == f'{self.prog_name} reboot':
+                my_job = job
+
+        # # create new job if neccesary
+        if my_job is None:
+            my_job = my_cron.new(command=cron_cmd,
+                                 comment=f'{self.prog_name} reboot')
+
+            # set job time
+            my_job.enable()
+            my_job.every_reboot()
+
+        # save job parameters
+        my_cron.write()
+
+    # --------------------------------------------------------------------------
+    # Run the program after install
+    # --------------------------------------------------------------------------
+    # def __run_prog(self):
+
+    #     # show some text
+    #     print(f'Running {self.prog_name}')
+
+    #     # run program now
+    #     cmd_array = shlex.split(self.run_cmd)
+    #     try:
+    #         subprocess.run(cmd_array)
+    #     except Exception as error:
+    #         print(f'Could not run {self.prog_name}:', error.stderr.decode())
+    #         exit()
+
+
+# ------------------------------------------------------------------------------
+# Run the main class if we are not an import
+# ------------------------------------------------------------------------------
+if __name__ == '__main__':
+    installer = CronInstaller()
+    installer.run()
+
+# -)
