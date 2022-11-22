0575ed552342206b18857ea487f2b9c171bc905a
Dana Hynes <46582023+danahynes@users.noreply.github.com>
2022-11-22 01:40:14 -0500

    new file

---
+++ src/__init__.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------
+# Project : SpaceOddity                                            /          \
+# Filename: __init__.py                                           |     ()     |
+# Date    : 10/03/2022                                            |            |
+# Author  : cyclopticnerve                                        |   \____/   |
+# License : WTFPLv2                                                \          /
+# ------------------------------------------------------------------------------
+
+__version__ = "0.1.0"
+
+'''
+A program that changes your wallpaper to NASA's Astronomy Picture of the Day
+'''
+
+# -)
